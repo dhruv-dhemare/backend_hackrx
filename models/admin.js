01e0edc716d9e315b14ec252f8ce08a7bd8bec00
dhruv-dhemare <dhruvdhemare@gmail.com>
2025-08-12 14:47:24 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,60 +1,47 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-// Define a subdocument schema for files
 const documentSchema = new mongoose.Schema({
   name: String,
-  data: Buffer,
+  path: String,          // to store file path
   contentType: String,
   uploadedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-// Extend your existing admin schema
 const adminSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  documents: [documentSchema]  // New field to store multiple documents
+  documents: [documentSchema],
 });
 
-// userSchema.pre('save',async function(next){
-//     const user = this;
+// Hash password before saving
+adminSchema.pre('save', async function (next) {
+  const admin = this;
+  if (!admin.isModified('password')) return next();
 
-//     //Hash the password only if it has been modified(or is new)
-//     if (!user.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    admin.password = await bcrypt.hash(admin.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
-//     try{
-//         //hash password generation
-//         const salt = await bcrypt.genSalt(10);
+// Compare password method
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
-//         //hash password
-//         const hashedPassword = await bcrypt.hash(user.password,salt);
-
-//         //override the plain password with the hashed one
-//         user.password = hashedPassword;
-
-//         next();
-//     }catch(err){
-//         next(err);
-//     }
-// })
-// userSchema.methods.comparePassword = async function(candidatePassword){
-//     try{
-//         // use bcrypt to compare the provided password with the hashed password
-//         const isMatch = await bcrypt.compare(candidatePassword, this.password);
-//         return isMatch;
-//     }catch(err){
-//         throw err;
-//     }
-// }
-
-//create person model
-const admin = mongoose.model('admin',adminSchema);
-module.exports = admin;
+const Admin = mongoose.model('Admin', adminSchema);
+module.exports = Admin;
