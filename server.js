c04ef684e8691b888bf27d017004e7aa5ea4c292
dhruv-dhemare <dhruvdhemare@gmail.com>
2025-08-12 12:57:20 +0530

    backenmd

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ require("dotenv").config();
 const app = express();
 const db = require('./db'); // now MONGO_URL will be available
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://hackrx-delulu-guys.netlify.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your React port (default for Vite)
-    credentials: true
+  origin: function(origin, callback){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
 
 const PORT = process.env.PORT || 3000;
 
