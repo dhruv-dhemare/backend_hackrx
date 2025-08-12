6a37fe1684d884105c2191bcc82fc4c0419fdc7c
dhruv-dhemare <dhruvdhemare@gmail.com>
2025-08-12 14:51:17 +0530

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,47 +1,15 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-const documentSchema = new mongoose.Schema({
-  name: String,
-  path: String,          // to store file path
-  contentType: String,
-  uploadedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
 
 const adminSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  documents: [documentSchema],
+  username: { type: String, required: true },
+  password: { type: String, required: true },  // store plain text (insecure)
+  documents: [{
+    name: String,
+    data: Buffer,
+    contentType: String,
+    uploadedAt: { type: Date, default: Date.now }
+  }]
 });
 
-// Hash password before saving
-adminSchema.pre('save', async function (next) {
-  const admin = this;
-  if (!admin.isModified('password')) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    admin.password = await bcrypt.hash(admin.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Compare password method
-adminSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
-
-const Admin = mongoose.model('Admin', adminSchema);
-module.exports = Admin;
+const admin = mongoose.model('admin', adminSchema);
+module.exports = admin;
